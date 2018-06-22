f01ca6c99e19a6c65d37a3d16f5201b93b979f99
ALMUNIA HERRANZ Alejandro <alejandro.almuniaherranz@soprasteria.com>
2018-06-22 09:15:09 +0200

    Hasta lección 14, v_for, tercera parte

---
+++ v_for.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var vm = new Vue({
+	el: 'main',
+	data: {
+		ciudades: [
+			{ nombre: 'Madrid' },
+			{ nombre: 'Barcelona' },
+			{ nombre: 'Valencia' },
+			{ nombre: 'Bilbo' },
+			{ nombre: 'Donosti' },
+			{ nombre: 'Lleida' },
+			{ nombre: 'A Corunha' },
+			{ nombre: 'O Grove' },
+			{ nombre: 'L\'Alacantí' }
+		]
+	}
+});
---
+++ v_for_three.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var vm = new Vue({
+	el: 'main',
+	data: {
+		datosUsuario: {
+			nombre: 'Alejandro',
+			apellidos: 'Almunia Herranz',
+			edad: 40,
+			sexo: 'Varonil y sexy',
+			poblacion: 'Magerit',
+			ciudad: 'Magerit',
+			pais: 'Sefarad'
+		}
+	}
+});
