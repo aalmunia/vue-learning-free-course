942896423d6caaa6c1921d43cade5fa24e6810bf
ALMUNIA HERRANZ Alejandro <alejandro.almuniaherranz@soprasteria.com>
2018-06-22 09:52:43 +0200

    Hasta leccion 16, v-bind

---
+++ v_bind.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var vm = new Vue({
+	el: 'main',
+	data: {
+		mensaje: 'Datos asociados de manera dinámica',
+		srcImagen: 'http://www.irishnews.com/picturesarchive/irishnews/irishnews/2017/07/16/094047243-092f6561-9f39-4dc6-a6b4-68f18879353d.jpg',
+		claseRojo: true,
+		nuevoBorde: true		
+	}	
+});
